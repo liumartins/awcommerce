4d31e42d15fabbfe295aa6d136a943fcfb63ef66
lindomar <lsmartins@awcommerce.com.br>
2019-01-04 13:19:57 -0200

    removendo secoes home

--- wp-content/themes/Parallax-One/front-page.php
+++ wp-content/themes/Parallax-One/front-page.php
@@ -22,8 +22,8 @@ if ( 'posts' == get_option( 'show_on_front' ) ) {
 	<div itemprop id="content" class="content-warp" role="main">
 	<?php parallax_hook_content_top(); ?>
 	<?php
-
-		$sections_array = apply_filters( 'parallax_one_plus_sections_filter', array( 'sections/parallax_one_logos_section', 'sections/parallax_one_our_services_section', 'sections/parallax_one_our_story_section', 'sections/parallax_one_our_team_section', 'sections/parallax_one_happy_customers_section', 'sections/parallax_one_ribbon_section', 'sections/parallax_one_latest_news_section', 'sections/parallax_one_contact_info_section', 'sections/parallax_one_map_section' ) );
+        //$sections_array = apply_filters( 'parallax_one_plus_sections_filter', array( 'sections/parallax_one_logos_section', 'sections/parallax_one_our_services_section', 'sections/parallax_one_our_story_section', 'sections/parallax_one_our_team_section', 'sections/parallax_one_happy_customers_section', 'sections/parallax_one_ribbon_section', 'sections/parallax_one_latest_news_section', 'sections/parallax_one_contact_info_section', 'sections/parallax_one_map_section' ) );
+		$sections_array = apply_filters( 'parallax_one_plus_sections_filter', array( 'sections/parallax_one_logos_section', 'sections/parallax_one_our_services_section','sections/parallax_one_ribbon_section', 'sections/parallax_one_latest_news_section', 'sections/parallax_one_contact_info_section', 'sections/parallax_one_map_section' ) );
 
 	if ( ! empty( $sections_array ) ) {
 		foreach ( $sections_array as $section ) {
